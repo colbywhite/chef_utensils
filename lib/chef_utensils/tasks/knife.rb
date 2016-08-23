de750f8a7ce249cd2c1c495c8365010f5cc9bdd8
Colby M. White <colbywhite@users.noreply.github.com>
2016-08-23 15:16:51 -0500

    making the freeze optional

--- lib/chef_utensils/tasks/knife.rb
+++ lib/chef_utensils/tasks/knife.rb
@@ -2,12 +2,13 @@ require 'chef/cookbook/metadata'
 require 'chef/application/knife'
 
 desc 'Upload the cookbook to the chef server'
-task :upload, [:env] do |_, args|
+task :upload, [:env, :freeze] do |_, args|
   metadata = Chef::Cookbook::Metadata.new
   metadata.from_file 'metadata.rb'
   knife_args = %w(cookbook upload)
   knife_args += [metadata.name]
-  knife_args += ['--freeze', '-E', args[:env]] if args[:env]
+  knife_args += ['-E', args[:env]] if args[:env]
+  knife_args += ['--freeze'] if args[:freeze]
   Chef::Log.init $stdout
   Chef::Log.level :info
   Chef::Log.info "+ knife #{knife_args.join ' '}"
