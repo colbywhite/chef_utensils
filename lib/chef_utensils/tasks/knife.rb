d992f0a0b50f2854d2d08eee595caafd9be42d18
Colby M. White <colbywhite@users.noreply.github.com>
2016-08-24 16:19:36 -0500

    removing env option
    opting for specifying version constraints in the env files themselves

--- lib/chef_utensils/tasks/knife.rb
+++ lib/chef_utensils/tasks/knife.rb
@@ -2,12 +2,11 @@ require 'chef/cookbook/metadata'
 require 'chef/application/knife'
 
 desc 'Upload the cookbook to the chef server'
-task :upload, [:env, :freeze] do |_, args|
+task :upload, [:freeze] do |_, args|
   metadata = Chef::Cookbook::Metadata.new
   metadata.from_file 'metadata.rb'
   knife_args = %w(cookbook upload)
   knife_args += [metadata.name]
-  knife_args += ['-E', args[:env]] if args[:env]
   knife_args += ['--freeze'] if args[:freeze] == 'true'
   Chef::Log.init $stdout
   Chef::Log.level :info
